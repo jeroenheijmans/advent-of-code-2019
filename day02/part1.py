fd8681b0464a15474bafe7bf1129fcb99e098f4e
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-02 06:18:00 +0100

    Add day 02 part 1 solution

--- day02/part1.py
+++ day02/part1.py
@@ -1,7 +1,22 @@
 with open('input.txt', 'r') as file:
-  data = file.read().splitlines()
+  data = list(map(int, file.read().splitlines()[0].split(",")))
 
-def solve(lines):
-  return 0
+def solve(program):
+  program[1] = 12
+  program[2] = 2
+  
+  i = 0
+
+  while True:
+    if program[i] == 1:
+      #print('add', program[program[i+3]], program[program[i+2]] , program[program[i+1]])
+      program[program[i+3]] = program[program[i+2]] + program[program[i+1]]
+    elif program[i] == 2:
+      #print('mul', program[program[i+3]], program[program[i+2]] , program[program[i+1]])
+      program[program[i+3]] = program[program[i+2]] * program[program[i+1]]
+    elif program[i] == 99:
+      return program[0]
+
+    i += 4
 
 print(solve(data))
