a3a5fd49b7efd4739d4475396996629e5094009e
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-03 06:18:40 +0100

    Add day 03 part 1 solution

--- day03/part1.py
+++ day03/part1.py
@@ -1,7 +1,28 @@
 with open('input.txt', 'r') as file:
   data = file.read().splitlines()
 
+def lineToTupleSet(line):
+  points = set()
+  x = 0
+  y = 0
+  for instruction in line.split(","):
+    direction = instruction[0]
+    length = int(instruction[1:])
+    for _ in range(0, length):
+      if direction == "U": y -= 1
+      if direction == "R": x += 1
+      if direction == "D": y += 1
+      if direction == "L": x -= 1
+      points.add(tuple([x, y]))
+  return points
+
+def distance(p1):
+  return abs(p1[0]) + abs(p1[1])
+
 def solve(input):
-  return 0
+  points1 = lineToTupleSet(input[0])
+  points2 = lineToTupleSet(input[1])
+  crossings = points1.intersection(points2)
+  return min(map(distance, crossings))
 
 print(solve(data))
