e40e4fdce736d9fe19c7307cff834108297eb78f
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-04 06:32:21 +0100

    Add day 04 part 2 solution (incorrect)

--- day04/part2.py
+++ day04/part2.py
@@ -2,6 +2,34 @@ with open('input.txt', 'r') as file:
   data = file.read().splitlines()
 
 def solve(input):
-  return 0
+  lower, upper = map(int, input[0].split("-"))
+  n = 0
 
+  for x in range(lower, upper):
+    candidate = str(x)
+    twosame = False
+    increases = True
+    nogroups = True
+    prev = candidate[0]
+    samecount = 0
+    
+    for i in range(1, len(candidate)):
+      if candidate[i] != prev:
+        if samecount == 1: twosame = True
+        samecount = 0
+
+      if candidate[i] == prev:
+        samecount += 1
+        
+      if candidate[i] < candidate[i - 1]:
+        increases = False
+        break
+
+    if twosame and increases and nogroups:
+      
+      n += 1
+
+  return n
+
+# Not 251
 print(solve(data))
