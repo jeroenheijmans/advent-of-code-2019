2f0bd6453f4aa1cdf4498910a1f89d00487a4782
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-04 06:23:11 +0100

    Add day 04 part 1 solution

--- day04/part1.py
+++ day04/part1.py
@@ -2,29 +2,23 @@ with open('input.txt', 'r') as file:
   data = file.read().splitlines()
 
 def solve(input):
-  data = input[0].split("-")
-  lower = int(data[0])
-  upper = int(data[1])
+  lower, upper = map(int, input[0].split("-"))
   n = 0
 
   for x in range(lower, upper):
     candidate = str(x)
     twosame = False
     increases = True
-    for i in range(1, len(candidate) - 1):
+    
+    for i in range(1, len(candidate)):
       if candidate[i] == candidate[i - 1]:
         twosame = True
       if candidate[i] < candidate[i - 1]:
         increases = False
         break
     if twosame and increases:
-      print(candidate)
       n += 1
 
   return n
 
-# Not 8
-# Not 78876
-# Not 78850
-# Not 4980
 print(solve(data))
