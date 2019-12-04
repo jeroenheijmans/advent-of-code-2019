f13f743cb0f258335ca222e5fd8650544662979c
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-04 06:08:48 +0100

    Add day 04 part 1 solution (WRONG)

--- day04/part1.py
+++ day04/part1.py
@@ -2,6 +2,24 @@ with open('input.txt', 'r') as file:
   data = file.read().splitlines()
 
 def solve(input):
-  return 0
+  data = input[0].split("-")
+  lower = int(data[0])
+  upper = int(data[1])
+  n = 0
 
+  for x in range(lower, upper + 1):
+    candidate = str(x)
+    okay = True
+    for i in range(len(candidate) - 1):
+      if candidate[i] == candidate[i + 1]:
+        okay = False
+        break
+      if candidate[i] > candidate[i + 1]:
+        okay = False
+        break
+    if okay: n += 1
+
+  return n
+
+# Not 8
 print(solve(data))
