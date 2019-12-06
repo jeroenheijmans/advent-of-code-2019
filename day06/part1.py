37e6b86f292abb5e2b9b81946701d06576c69c11
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-06 06:44:11 +0100

    Add day 06 part 1 solution

--- day06/part1.py
+++ day06/part1.py
@@ -2,6 +2,36 @@ with open('input.txt', 'r') as file:
   data = file.read().splitlines()
 
 def solve(input):
-  return 'no result found'
+  entries = list(map(lambda x: (x.split(")")[0], x.split(")")[1]), input))
+
+  orbits = dict()
+
+  for a,b in entries:
+    if a in orbits: orbits[a].add(b)
+    else: orbits[a] = set([b])
+
+  # print(orbits)
+
+  keepGoing = True
+  loop = 0
+
+  while keepGoing:
+    keepGoing = False
+    loop += 1
+    print(loop)
+
+    for keyA in orbits:
+      for val in orbits[keyA].copy():
+        if not val in orbits:
+          continue
+        for candidate in orbits[val]:
+          if candidate != val and candidate not in orbits[keyA]:
+            keepGoing = True
+            # print('appending', candidate, 'to', keyA)
+            orbits[keyA].add(candidate)
+
+  # print(orbits)
+
+  return sum(map(len, orbits.values()))
 
 print(solve(data))
