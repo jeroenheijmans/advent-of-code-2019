84d8bb1a934c201ca5d4a01d95a91d0eab1db501
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-06 08:01:28 +0100

    Add day 06 part 2 solution

--- day06/part2.py
+++ day06/part2.py
@@ -1,45 +1,19 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 with open('input.txt', 'r') as file:
   data = file.read().splitlines()
 
 def solve(input):
   entries = list(map(lambda x: (x.split(")")[0], x.split(")")[1]), input))
-
-  orbits = dict()
+  graph = nx.Graph()
 
   for a,b in entries:
-    if a in orbits: orbits[a].add(b)
-    else: orbits[a] = set([b])
-    if b in orbits: orbits[b].add(a)
-    else: orbits[b] = set([a])
+    graph.add_edge(a,b)
 
-  # print(orbits)
+  # nx.draw(graph)
+  # plt.show()
 
-  keepGoing = True
-  loop = 1
+  return nx.shortest_path_length(graph, "YOU", "SAN") - 2
 
-  paths = set(["YOU"])
-
-  while keepGoing and loop < 50000:
-    keepGoing = False
-    loop += 1
-
-    newPaths = paths.copy()
-
-    for path in paths:
-      tail = path[-3:]
-      for step in orbits[tail]:
-        if step not in path:
-          newPath = f"{path}{step}"
-          # print(newPath)
-          if newPath not in newPaths:
-            if step == "SAN": print(f"{path}SAN")
-            keepGoing = True
-            newPaths.add(newPath)
-
-    paths = newPaths
-
-  print('after', loop, 'iterations')
-  return paths
-
-result = solve(data)
-# print(result)
+print(solve(data))
