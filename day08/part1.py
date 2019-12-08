1ef62c309af5c99106b2122b7102e45891b3fd7e
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-08 06:21:10 +0100

    Add day 08 part 1 solution (INCORRECT)

--- day08/part1.py
+++ day08/part1.py
@@ -1,7 +1,28 @@
 with open('input.txt', 'r') as file:
-  data = file.read().splitlines()
+  data = list(map(int, file.read().splitlines()[0]))
 
 def solve(data):
-  return 'Not found'
+  # matrix = [[-1 for x in range(w)] for y in range(h)]
 
+  size = 25 * 6
+  nr = len(data) // size
+  i = 0
+  lowest = None
+
+  for layer in range(nr):
+    nr1 = 0
+    nr2 = 0
+    for _ in range(size):
+      if data[i] == 1: nr1 += 1
+      if data[i] == 2: nr2 += 1
+      i += 1
+
+    checksum = nr1 * nr2
+    lowest = checksum if lowest is None else min(lowest, checksum)
+    print(layer, checksum)
+    
+
+  return 'found', lowest
+
+# no 774
 print(solve(data))
