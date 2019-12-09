123307e2590eddb695a3a92a4ed6e2530782dcdf
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-09 20:54:39 +0100

    Add generator cheat sheet code

---
+++ day09/generator-edition.py
@@ -0,0 +1,86 @@
+from collections import defaultdict
+
+def runComputer(data, input):
+  program = defaultdict(int, { k: v for k, v in enumerate(data) })
+  output = None
+  i = 0
+  relbase = 0
+
+  while True:
+    opcode = program[i] % 100
+
+    if opcode == 99:
+      break
+
+    mode1 = (program[i] - opcode) // 100 % 10
+    mode2 = (program[i] - opcode) // 1000 % 10
+    mode3 = (program[i] - opcode) // 10000 % 10
+
+    p1, p2, p3 = None, None, None
+
+    if mode1 == 0: p1 = program[i + 1]
+    elif mode1 == 1: p1 = i + 1
+    elif mode1 == 2: p1 = program[i + 1] + relbase
+
+    if mode2 == 0: p2 = program[i + 2]
+    elif mode2 == 1: p2 = i + 2
+    elif mode2 == 2: p2 = program[i + 2] + relbase
+  
+    if mode3 == 0: p3 = program[i + 3]
+    elif mode3 == 1: raise ValueError('Immediate mode invalid for param 3')
+    elif mode3 == 2: p3 = program[i + 3] + relbase
+
+    print('i =', i, '--- operation', opcode, '--- modes', mode1, mode2, mode3, '--- positions', str(p1).rjust(4, ' '), str(p2).rjust(4, ' '), str(p3).rjust(4, ' '))
+
+    if opcode == 1: # addition
+      program[p3] = program[p1] + program[p2]
+      i += 4
+    elif opcode == 2: # multiplication
+      program[p3] = program[p1] * program[p2]
+      i += 4
+    elif opcode == 3: # input
+      program[p1] = input.pop()
+      i += 2
+    elif opcode == 4: # output
+      yield program[p1]
+      i += 2
+    elif opcode == 5: # jump-if-true
+      i = program[p2] if program[p1] != 0 else i + 3
+    elif opcode == 6: # jump-if-false
+      i = program[p2] if program[p1] == 0 else i + 3
+    elif opcode == 7: # less-than
+      program[p3] = 1 if program[p1] < program[p2] else 0
+      i += 4
+    elif opcode == 8: # equals
+      program[p3] = 1 if program[p1] == program[p2] else 0
+      i += 4
+    elif opcode == 9: # relative base adjust
+      relbase += program[p1]
+      i += 2
+    else:
+      raise ValueError(f'opcode {opcode} from {program[i]}')
+
+# Testrun:
+program = [3,1000,4,1000,3,1001,4,1001,99]
+input = [888]
+result = runComputer(program, input)
+print(next(result))
+input.append(999)
+print(next(result))
+
+
+# Bonus code for testing:
+def run(program, input):
+  memory = 0
+  i = 0
+  while True:
+    if program[i] == 'out': yield memory
+    if program[i] == 'in': memory = input.pop()
+    i = (i + 1) % len(program)
+
+program = ['in', 'out', 'in', 'out', 'out', 'in']
+inputs = [1]
+runner = run(program, inputs)
+print(next(runner))
+inputs.append(2)
+print(next(runner))
