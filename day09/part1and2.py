1469cda60910659a49a92a4f736a5cfdfb54e9cc
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-09 19:57:30 +0100

    Clean up day 09 some more

--- day09/part1and2.py
+++ day09/part1and2.py
@@ -19,45 +19,44 @@ def runComputer(data, input):
     mode2 = (program[i] - opcode) // 1000 % 10
     mode3 = (program[i] - opcode) // 10000 % 10
 
-    param1, param2, param3 = None, None, None
+    p1, p2, p3 = None, None, None
 
-    if mode1 == 0: param1 = program[program[i+1]]
-    elif mode1 == 1: param1 = program[i+1]
-    elif mode1 == 2: param1 = program[program[i+1] + relbase]
+    if mode1 == 0: p1 = program[i + 1]
+    elif mode1 == 1: p1 = i + 1
+    elif mode1 == 2: p1 = program[i + 1] + relbase
 
-    if mode2 == 0: param2 = program[program[i+2]]
-    elif mode2 == 1: param2 = program[i+2]
-    elif mode2 == 2: param2 = program[program[i+2] + relbase]
+    if mode2 == 0: p2 = program[i + 2]
+    elif mode2 == 1: p2 = i + 2
+    elif mode2 == 2: p2 = program[i + 2] + relbase
   
-    if mode3 == 0: param3 = program[i+3]
+    if mode3 == 0: p3 = program[i + 3]
     elif mode3 == 1: raise ValueError('Immediate mode invalid for param 3')
-    elif mode3 == 2: param3 = program[i+3] + relbase
+    elif mode3 == 2: p3 = program[i + 3] + relbase
 
     if opcode == 1: # addition
-      program[param3] = param1 + param2
+      program[p3] = program[p1] + program[p2]
       i += 4
     elif opcode == 2: # multiplication
-      program[param3] = param1 * param2
+      program[p3] = program[p1] * program[p2]
       i += 4
     elif opcode == 3: # input
-      if mode1 == 2: param1 = program[i+1] + relbase
-      program[param1] = input
+      program[p1] = input
       i += 2
     elif opcode == 4: # output
-      output = param1
+      output = program[p1]
       i += 2
     elif opcode == 5: # jump-if-true
-      i = param2 if param1 != 0 else i+3
+      i = program[p2] if program[p1] != 0 else i + 3
     elif opcode == 6: # jump-if-false
-      i = param2 if param1 == 0 else i+3
+      i = program[p2] if program[p1] == 0 else i + 3
     elif opcode == 7: # less-than
-      program[param3] = 1 if param1 < param2 else 0
+      program[p3] = 1 if program[p1] < program[p2] else 0
       i += 4
     elif opcode == 8: # equals
-      program[param3] = 1 if param1 == param2 else 0
+      program[p3] = 1 if program[p1] == program[p2] else 0
       i += 4
     elif opcode == 9: # relative base adjust
-      relbase += param1
+      relbase += program[p1]
       i += 2
     else:
       raise ValueError(f'opcode {opcode} from {program[i]}')
