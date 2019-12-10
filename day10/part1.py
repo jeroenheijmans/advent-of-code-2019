cfebd5ddcc3504c11cfedeadbe9c9858898f9f63
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-10 06:37:42 +0100

    Add day 10 part 1 solution

--- day10/part1.py
+++ day10/part1.py
@@ -1,7 +1,32 @@
+import math
+
 with open('input.txt', 'r') as file:
-  data = file.read().splitlines()
+  data = list(file.read().splitlines())
 
 def solve(input):
-  return 'No solution found'
+  points = dict()
+  x, y = -1, -1
+
+  for line in input:
+    y += 1
+    x = -1
+    for char in line:
+      x += 1
+      if char == "#":
+        points[(x,y)] = dict()
+  
+  for p in points.keys():
+    for other in points.keys():
+      if p == other: continue
+      rad = math.atan2(other[1]-p[1], other[0]-p[0])
+      key = int(rad * 100000) + 1
+      if key not in points[p]:
+        points[p][key] = list()
+      points[p][key].append(other)
+    #print(p, points[p], len(points[p]))
+
+  result = max(points, key = lambda p: len(points[p].values()))
+
+  return result, ' visible: ', len(points[result].values())
 
 print(solve(data))
