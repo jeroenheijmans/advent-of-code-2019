093960a5cc5cc1b85427903597c038d98c476c20
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-12 21:38:33 +0100

    Update day 12 part 2

--- day12/part2.py
+++ day12/part2.py
@@ -1,24 +1,15 @@
 from time import time
-from itertools import permutations
+from itertools import permutations, combinations
 
 with open('input.txt', 'r') as file:
   data = list(file.read().splitlines())
-
-def energy(positions, velocities):
-  energy = 0
-  for one in range(4):
-    pot = abs(positions[one][0]) + abs(positions[one][1]) + abs(positions[one][2])
-    kin = abs(velocities[one][0]) + abs(velocities[one][1]) + abs(velocities[one][2])
-    total = pot * kin
-    energy += total
-  return energy
 
 def solve(input):
   # Example 1
   positions = [(-1, 0, 2), (2, -10, -7), (4, -8, 8), (3, 5, -1)]
 
   # Example 2
-  # positions = [(-8, -10, 0), (5, 5, 10), (2, -7, 3), (9, -8, -3)]
+  positions = [(-8, -10, 0), (5, 5, 10), (2, -7, 3), (9, -8, -3)]
 
   # My input:
   #positions = [(-8, -9,  -7), (-5,  2,  -1), (11,  8, -14), ( 1, -4, -11)]
@@ -29,27 +20,20 @@ def solve(input):
   startvelocities = velocities.copy()
 
   for step in range(4_686_774_924 + 1):
-    vchanges = [(0,0,0), (0,0,0), (0,0,0), (0,0,0)]
-
-    for one, two in permutations([0, 1, 2, 3], 2):
-      v1 = velocities[one]
+    for one, two in combinations([0, 1, 2, 3], 2):
       p1 = positions[one]
       p2 = positions[two]
 
-      dx = 1 if p1[0] < p2[0] else -1 if p1[0] > p2[0] else 0
-      dy = 1 if p1[1] < p2[1] else -1 if p1[1] > p2[1] else 0
-      dz = 1 if p1[2] < p2[2] else -1 if p1[2] > p2[2] else 0
+      factor = 1
 
-      vc1 = vchanges[one]
-      vchanges[one] = (vc1[0] + dx, vc1[1] + dy, vc1[2] + dz)
+      dx = factor if p1[0] < p2[0] else -factor if p1[0] > p2[0] else 0
+      dy = factor if p1[1] < p2[1] else -factor if p1[1] > p2[1] else 0
+      dz = factor if p1[2] < p2[2] else -factor if p1[2] > p2[2] else 0
 
-    for one in range(4):
       v1 = velocities[one]
-      velocities[one] = (
-        v1[0] + vchanges[one][0],
-        v1[1] + vchanges[one][1],
-        v1[2] + vchanges[one][2]
-      )
+      v2 = velocities[two]
+      velocities[one] = (v1[0] + dx, v1[1] + dy, v1[2] + dz)
+      velocities[two] = (v2[0] - dx, v2[1] - dy, v2[2] - dz)
 
     for one in range(4):
       p1 = positions[one]
@@ -69,7 +53,7 @@ def solve(input):
       break
 
     if step % 1e5 == 0:
-      print(step, ",", round(time() - start, 3))
+      print(step, ",", round(time() - start, 3), positions)
 
   return step + 1
 
