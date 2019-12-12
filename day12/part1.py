e1bf821df6665e43e114c547205544d8ec1dd328
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-12 07:29:53 +0100

    Clean up day 12 part 1 some more

--- day12/part1.py
+++ day12/part1.py
@@ -1,94 +1,40 @@
-from itertools import combinations
+from itertools import permutations
 
 with open('input.txt', 'r') as file:
   data = list(file.read().splitlines())
 
 def solve(input):
-  # Example 1
-  positions = [
-    (-1, 0, 2),
-    (2, -10, -7),
-    (4, -8, 8),
-    (3, 5, -1)
-  ]
+  positions = [(-8, -9,  -7), (-5,  2,  -1), (11,  8, -14), ( 1, -4, -11)]
+  velocities = [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)]
 
-  # Example 2
-  positions = [
-    (-8, -10, 0),
-    (5, 5, 10),
-    (2, -7, 3),
-    (9, -8, -3)
-  ]
-  
-  # Own example
-  # positions = [
-  #   (1, 0, 0),
-  #   (0, 0, 0),
-  #   (0, 0, 0),
-  #   (0, 0, 0)
-  # ]
+  for _ in range(1000):
+    for one, two in permutations([0, 1, 2, 3], 2):
+      v1 = velocities[one]
+      p1 = positions[one]
+      p2 = positions[two]
 
-  # My input:
-  positions = [
-    (-8, -9,  -7),
-    (-5,  2,  -1),
-    (11,  8, -14),
-    ( 1, -4, -11)
-  ]
+      dx = 1 if p1[0] < p2[0] else -1 if p1[0] > p2[0] else 0
+      dy = 1 if p1[1] < p2[1] else -1 if p1[1] > p2[1] else 0
+      dz = 1 if p1[2] < p2[2] else -1 if p1[2] > p2[2] else 0
 
-  velocities = [
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0)
-  ]
+      velocities[one] = (v1[0] + dx, v1[1] + dy, v1[2] + dz)
 
-  for step in range(1000):
-    print(
-      step,
-      '=',
-      list(map(lambda p: (str(p[0]).rjust(3), str(p[1]).rjust(3), str(p[2]).rjust(3)), positions)),
-      'vel',
-      list(map(lambda p: (str(p[0]).rjust(3), str(p[1]).rjust(3), str(p[2]).rjust(3)), velocities)),
-    )
-
-    for one in range(len(positions)):
-      for two in range(len(positions)):
-        if one == two: continue
-
-        if positions[one][0] < positions[two][0]: dx = 1
-        elif positions[one][0] > positions[two][0]: dx = -1
-        else: dx = 0
-        
-        if positions[one][1] < positions[two][1]: dy = 1
-        elif positions[one][1] > positions[two][1]: dy = -1
-        else: dy = 0
-        
-        if positions[one][2] < positions[two][2]: dz = 1
-        elif positions[one][2] > positions[two][2]: dz = -1
-        else: dz = 0
-
-        velocities[one] = (
-          velocities[one][0] + dx,
-          velocities[one][1] + dy,
-          velocities[one][2] + dz
-        )
-
-    for one in range(len(positions)):
+    for one in range(4):
+      p1 = positions[one]
+      v1 = velocities[one]
       positions[one] = (
-        positions[one][0] + velocities[one][0],
-        positions[one][1] + velocities[one][1],
-        positions[one][2] + velocities[one][2]
+        p1[0] + v1[0],
+        p1[1] + v1[1],
+        p1[2] + v1[2]
       )
 
   energy = 0
 
-  for one in range(len(positions)):
+  for one in range(4):
     pot = abs(positions[one][0]) + abs(positions[one][1]) + abs(positions[one][2])
     kin = abs(velocities[one][0]) + abs(velocities[one][1]) + abs(velocities[one][2])
     total = pot * kin
     energy += total
-    # print(pot, '*', kin, '=', total)
 
   return energy
 
