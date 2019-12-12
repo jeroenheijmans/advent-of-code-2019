fd0e63eb82755e5876d9c13214817108b6bbecc0
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-12 06:15:38 +0100

    Add day 12 part 1 solution (incorrect)

--- day12/part1.py
+++ day12/part1.py
@@ -1,9 +1,50 @@
+from itertools import combinations
+
 with open('input.txt', 'r') as file:
   data = list(file.read().splitlines())
 
 def solve(input):
+  positions = [
+    (-8, -9,  -7),
+    (-5,  2,  -1),
+    (11,  8, -14),
+    ( 1, -4, -11)
+  ]
 
+  velocities = [
+    (0, 0, 0),
+    (0, 0, 0),
+    (0, 0, 0),
+    (0, 0, 0)
+  ]
 
-  return 'Done!'
+  for step in range(1000):
+    newpositions = []
+    for one in range(len(positions)):
+      for two in range(len(positions)):
+        if one == two: continue
+        velocities[one] = (
+          (1 if positions[one][0] < positions[two][0] else (1 if positions[one][0] > positions[two][0] else 0)),
+          (1 if positions[one][1] < positions[two][1] else (1 if positions[one][1] > positions[two][1] else 0)),
+          (1 if positions[one][2] < positions[two][2] else (1 if positions[one][2] > positions[two][2] else 0))
+        )
 
+    for one in range(len(positions)):
+      positions[one] = (
+        positions[one][0] + velocities[one][0],
+        positions[one][1] + velocities[one][1],
+        positions[one][2] + velocities[one][2]
+      )
+
+  energy = 0
+
+  for one in range(len(positions)):
+    pot = positions[one][0] + positions[one][1] + positions[one][2]
+    kin = velocities[one][0] + velocities[one][1] + velocities[one][2]
+    total = pot * kin
+    energy += total
+
+  return energy
+
+# Not 35889
 print(solve(data))
