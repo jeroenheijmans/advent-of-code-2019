2b1e15a64bf964ff279d26e923829bc8f01a4c5e
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-14 18:58:28 +0100

    Add day 12 part 2 debug info

--- day12/part2.py
+++ day12/part2.py
@@ -10,23 +10,33 @@ def energy(vector):
 def energies(vectors):
   return sum(map(energy, vectors))
 
+def total(positions, velocities):
+  return sum([energy(positions[i]) * energy(velocities[i])  for i in range(4)])
+    
+
 def solve(input):
-  # Example 1
+  # Example 1 (Expected: 2772)
   positions = [(-1, 0, 2), (2, -10, -7), (4, -8, 8), (3, 5, -1)]
 
-  # Example 2
-  positions = [(-8, -10, 0), (5, 5, 10), (2, -7, 3), (9, -8, -3)]
+  # Example 2 (expected: 4_686_774_924)
+  # positions = [(-8, -10, 0), (5, 5, 10), (2, -7, 3), (9, -8, -3)]
 
-  # My input:
-  positions = [(-8, -9,  -7), (-5,  2,  -1), (11,  8, -14), ( 1, -4, -11)]
+  # My input (expected: puzzle answer :D)
+  # positions = [(-8, -9,  -7), (-5,  2,  -1), (11,  8, -14), ( 1, -4, -11)]
+
+  # Made up example (expected: 168)
+  positions = [(0,0,0), (1,0,0), (0,2,0), (3,4,5)]
 
   velocities = [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)]
   start = time()
   startpositions = positions.copy()
   startvelocities = velocities.copy()
   combis = [(0,1), (0,2), (0,3), (1,2), (1,3), (2,3)]
+  # es = []
 
   for step in range(4_686_774_924 + 16_000_000_000):
+    # es.append(",".join([str(energies(positions)), str(energies(velocities))]))
+
     for one, two in combis:
       p1 = positions[one]
       p2 = positions[two]
@@ -53,10 +63,13 @@ def solve(input):
       print('same!', round(time() - start, 4))
       print(positions)
       print(velocities)
+      # es.append(energies(velocities))
       break
 
     if step % 1e6 == 0:
-      print(step, ",", round(time() - start, 4), positions)
+      print(step, ",", str(round(time() - start, 4)).rjust(6), positions)
+
+  # print("\n".join([str(e) for e in es]))
 
   return step + 1
 
