d3cdc5efc96b6eae5b735bf096bc659a1515848e
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-14 15:34:36 +0100

    Prepare day 15

---
+++ day15/part1.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+with open('input.txt', 'r') as file:
+  data = file.read().splitlines()
+
+def solve(data):
+  return 'No solution found yet'
+
+print(solve(data))
