d296726f32306598e153cf6d9be2dfbefd0da547
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-14 05:57:29 +0100

    Add day 12 part 2 solution (still too slow)

--- day12/part2.py
+++ day12/part2.py
@@ -1,4 +1,3 @@
-from csv import writer
 from time import time
 from itertools import permutations, combinations
 
@@ -16,30 +15,28 @@ def solve(input):
   positions = [(-1, 0, 2), (2, -10, -7), (4, -8, 8), (3, 5, -1)]
 
   # Example 2
-  # positions = [(-8, -10, 0), (5, 5, 10), (2, -7, 3), (9, -8, -3)]
+  positions = [(-8, -10, 0), (5, 5, 10), (2, -7, 3), (9, -8, -3)]
 
   # My input:
-  # positions = [(-8, -9,  -7), (-5,  2,  -1), (11,  8, -14), ( 1, -4, -11)]
+  positions = [(-8, -9,  -7), (-5,  2,  -1), (11,  8, -14), ( 1, -4, -11)]
 
   velocities = [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)]
   start = time()
   startpositions = positions.copy()
   startvelocities = velocities.copy()
-  csv = []
+  combis = [(0,1), (0,2), (0,3), (1,2), (1,3), (2,3)]
 
-  for step in range(4_686_774_924 + 1_000_000_000):
-    csv.append([energies(positions), energies(velocities)])
-
-    for one, two in combinations([0, 1, 2, 3], 2):
+  for step in range(4_686_774_924 + 16_000_000_000):
+    for one, two in combis:
       p1 = positions[one]
       p2 = positions[two]
+      v1 = velocities[one]
+      v2 = velocities[two]
 
       dx = 1 if p1[0] < p2[0] else -1 if p1[0] > p2[0] else 0
       dy = 1 if p1[1] < p2[1] else -1 if p1[1] > p2[1] else 0
       dz = 1 if p1[2] < p2[2] else -1 if p1[2] > p2[2] else 0
 
-      v1 = velocities[one]
-      v2 = velocities[two]
       velocities[one] = (v1[0] + dx, v1[1] + dy, v1[2] + dz)
       velocities[two] = (v2[0] - dx, v2[1] - dy, v2[2] - dz)
 
@@ -53,21 +50,13 @@ def solve(input):
       )
     
     if positions == startpositions and velocities == startvelocities:
-      print('same!')
+      print('same!', round(time() - start, 4))
       print(positions)
-      print(startpositions)
       print(velocities)
-      print(startvelocities)
       break
 
-    if step % 1e5 == 0:
-      print(step, ",", round(time() - start, 3), positions)
-
-  csv.append([energies(positions), energies(velocities)])
-  
-  with open("out.csv", "w", newline="") as csvfile:
-    wr = writer(csvfile)
-    wr.writerows(csv)
+    if step % 1e6 == 0:
+      print(step, ",", round(time() - start, 4), positions)
 
   return step + 1
 
