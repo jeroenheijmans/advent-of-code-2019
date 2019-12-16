4e917986f066fe1747350af09bb64d64557d07de
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-16 19:41:35 +0100

    Add day 16 part 2 solution (part of it)

--- day16/part2.py
+++ day16/part2.py
@@ -7,51 +7,32 @@ def firsteight(freqs):
 officialRepeatCount = 10000
 
 def solve(data, messageRepeat = officialRepeatCount):
-  start = time()
-  data = data * messageRepeat
-  messageoffset = int(data[:7])
+  messageoffset = int(data[:7]) % len(data)
   freqs = list(map(int, data))
   pattern = [0, 1, 0, -1]
   maxlength = len(freqs)
 
-  print('Prepping factor matrix at time', str(round(time() - start, 4)).ljust(7, "0"))
-
-  factorMatrix = []
-  for i in range(maxlength):
-    repeats = i + 1
-    newlist = []
-    factorMatrix.append(newlist)
-    for j in range(maxlength):
-      pidx = (j + 1) // repeats
-      pidx = pidx % 4
-      newlist.append(pattern[pidx])
-
-
-  for step in range(100):
-    print(str(step).ljust(3, ' '), 'time', str(round(time() - start, 4)).ljust(7, "0"))
-
+  for _ in range(100):
     newfreqs = []
 
     for i in range(maxlength):
       newf = 0
-      repeats = i + 1
+      j = 0
       for j in range(maxlength):
-        factor = factorMatrix[i][j]
+        repeats = i + 1
+        pidx = (j + 1) // repeats
+        pidx = pidx % 4
+        factor = pattern[pidx]
         newf += freqs[j] * factor
         
       newfreqs.append(abs(newf) % 10)
     
     freqs = newfreqs
-
-  print('first 100 digits (full result in file)', ''.join(list(map(str, freqs[:100]))))
-  
-  with  open('temp.txt', 'w') as file:
-    file.write(''.join(list(map(str, freqs))))
   
   return firsteight(freqs[messageoffset:])
 
 with open('input.txt', 'r') as file:
   txt = file.read().splitlines()[0]
 
-# 55555555 wrong, too high (guessed based on output middle section)
+# 43852383 wrong, too high!
 print("Part 2:", solve(txt))
