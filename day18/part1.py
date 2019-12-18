124671e2d076bb1ec6a0066bd9b8a26c9093a2ff
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-18 18:18:45 +0100

    Add day 18 part 1 optimizations (final?)

--- day18/part1.py
+++ day18/part1.py
@@ -109,7 +109,7 @@ def createGameFrom(level, position):
     potentials = [x for x in curgraph.nodes() if len(list(curgraph.neighbors(x))) == 2]
     for pot in potentials:
       others = list(curgraph.neighbors(pot))
-      if pot in spaces and len(others) == 2 and others[0] in spaces and others[1] in spaces:
+      if pot in spaces and len(others) == 2:
         weight = curgraph.edges[others[0], pot]['weight'] + curgraph.edges[others[1], pot]['weight']
         curgraph.remove_node(pot)
         spaces.remove(pot)
