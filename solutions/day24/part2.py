818d69db5dad7ae872a86840dda28bb23b99b255
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-12-24 07:10:21 +0100

    Add day 24 part 2 solution (WRONG)

--- solutions/day24/part2.py
+++ solutions/day24/part2.py
@@ -1,21 +1,38 @@
-def draw(level):
-  size = 5 # assume a square
-  for y in range(size):
-    line = "".join([level[(x,y)] for x in range(size)])
-    print(line)
+def draw(levels):
+  mini = min(levels.keys())
+  maxi = max(levels.keys())
 
-def stringify(level):
-  return "".join(level.values())
+  for i in range(mini, maxi + 1):
+    print("\nLevel", i)
+    level = levels[i]
+    size = 5 # assume a square
+    for y in range(size):
+      line = "".join([level[(x,y)] for x in range(size)])
+      print(line)
 
-def neighbors(level, p):
-  return [p for p in [
-    (p[0] - 1, p[1]),
-    (p[0] + 1, p[1]),
-    (p[0], p[1] - 1),
-    (p[0], p[1] + 1),
-  ] if p in level]
+def neighbors(levels, rec, p):
+  ns = []
 
-def solve(raw):
+  if (p[0] - 1, p[1]) != (2,2) and (p[0] - 1, p[1]) in levels[rec]: ns.append(levels[rec][(p[0] - 1, p[1])])
+  if (p[0] + 1, p[1]) != (2,2) and (p[0] + 1, p[1]) in levels[rec]: ns.append(levels[rec][(p[0] + 1, p[1])])
+  if (p[0], p[1] - 1) != (2,2) and (p[0], p[1] - 1) in levels[rec]: ns.append(levels[rec][(p[0], p[1] - 1)])
+  if (p[0], p[1] + 1) != (2,2) and (p[0], p[1] + 1) in levels[rec]: ns.append(levels[rec][(p[0], p[1] + 1)])
+
+  if rec + 1 in levels:
+    if p[1] == 0: ns.append(levels[rec + 1][(2,1)]) # add 8 to A-E
+    if p[1] == 4: ns.append(levels[rec + 1][(2,3)]) # add 18 to U-Y
+    if p[0] == 0: ns.append(levels[rec + 1][(1,2)]) # add 12 to A/F/K/P/U
+    if p[0] == 4: ns.append(levels[rec + 1][(3,2)]) # add 14 to E/J/O/T/Y
+
+  if rec - 1 in levels:
+    if p == (2,1): ns.extend([levels[rec - 1][p] for p in levels[rec - 1] if p[1] == 0]) # add toprow to H
+    if p == (1,2): ns.extend([levels[rec - 1][p] for p in levels[rec - 1] if p[0] == 0]) # add leftrow to L
+    if p == (3,2): ns.extend([levels[rec - 1][p] for p in levels[rec - 1] if p[0] == 4]) # add right to N
+    if p == (2,3): ns.extend([levels[rec - 1][p] for p in levels[rec - 1] if p[0] == 4]) # add botrow to R
+
+  return ns
+
+def createlevel(data):
   level = dict()
 
   x,y = 0,0
@@ -25,31 +42,50 @@ def solve(raw):
       x += 1
     y += 1
     x = 0
+
+  return level
+
+
+def solve(raw):
+  levels = dict()
+  levels[0] = createlevel(raw)
+  empty = createlevel(["....."] * 5)
   
-  layouts = set()
+  for minutes in range(200):
+    print(f"At minute {minutes}")
+    newlevels = dict()
 
-  while True:
-    txt = stringify(level)
-    if txt in layouts: break
-    layouts.add(txt)
-    newlevel = dict()
-    for p in level:
-      ns = neighbors(level, p)
-      bugcount = sum([1 for n in ns if level[n] == "#"])
-      if level[p] == "#" and not bugcount == 1: newlevel[p] = "."
-      elif level[p] == "." and (bugcount == 1 or bugcount == 2): newlevel[p] = "#"
-      else: newlevel[p] = level[p]
-    level = newlevel
+    for depth in range(-minutes, minutes+1):
 
-  power = 1
+      if depth not in levels:
+        levels[depth] = empty.copy()
+
+      level = levels[depth]
+      newlevel = dict()
+      newlevels[depth] = newlevel
+
+      for p in level:
+        ns = neighbors(levels, depth, p)
+        bugcount = sum([1 for n in ns if n == "#"])
+        if level[p] == "#" and not bugcount == 1: newlevel[p] = "."
+        elif level[p] == "." and (bugcount == 1 or bugcount == 2): newlevel[p] = "#"
+        else: newlevel[p] = level[p]
+
+    levels = newlevels
+
+    # draw(levels)
+    # input()
+
+  # draw(levels)
+
   result = 0
-  for p in level:
-    if level[p] == "#": result += power
-    power = power * 2
+  for rec in range(min(levels.keys()), max(levels.keys())):
+    result += sum([1 for p in levels[rec] if levels[rec][p] == "#"])
 
   return result
 
 with open('input.txt', 'r') as file:
   raw = file.read().splitlines()
 
+# Not 4133
 print("Solution:", solve(raw))
